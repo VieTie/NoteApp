fc0f495978233e158343903153a023d8b053c364
VieTie0910 <109495007+VieTie0910@users.noreply.github.com>
2022-10-14 23:53:38 +0700

    update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/style/app.dart';
+
+import '../style/home.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,6 +13,101 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: lightColor,
+        appBar: theAppBar("Reminder"),
+        body: Container(
+          margin: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+          height: 48.0,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              label("All"),
+              const SizedBox(width: 16.0),
+              label("Home"),
+              const SizedBox(width: 16.0),
+              label("Task"),
+              const SizedBox(width: 16.0),
+              label("Shopping"),
+              const SizedBox(width: 16.0),
+              label("➕"),
+
+            ],
+          ),
+        ),
+        drawer: Drawer(
+            child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: <Color>[
+                          lightestMainColor,
+                          lighterMainColor,
+                          mainColor
+                        ],
+                        tileMode: TileMode.mirror)),
+                child: Padding(
+                    padding: EdgeInsets.zero,
+                    child: ListView(children: [
+                      SizedBox(
+                          height: 80.0,
+                          child: DrawerHeader(
+                              child: Center(
+                                  child: Text("T-Note",
+                                      style: TextStyle(
+                                          fontFamily: "Montserrat",
+                                          fontSize: 32,
+                                          color: darkColor))))),
+                      ListTile(
+                          leading: drawerIcon(Icons.event_note),
+                          title: drawerTitle("Notes"),
+                          onTap: () {
+                            Navigator.pop(context);
+                          }),
+                      const SizedBox(height: 8.0),
+                      ListTile(
+                          leading: drawerIcon(Icons.calendar_month),
+                          title: drawerTitle("Calendar"),
+                          onTap: () {
+                            Navigator.pop(context);
+                          }),
+                      const SizedBox(height: 8.0),
+                      ListTile(
+                          leading: drawerIcon(Icons.view_timeline),
+                          title: drawerTitle("Timeline"),
+                          onTap: () {
+                            Navigator.pop(context);
+                          }),
+                      const SizedBox(height: 8.0),
+                      ListTile(
+                          leading: drawerIcon(Icons.class_rounded),
+                          title: drawerTitle("Types"),
+                          onTap: () {
+                            Navigator.pop(context);
+                          }),
+                      const SizedBox(height: 8.0),
+                      ListTile(
+                          leading:
+                              drawerIcon(Icons.domain_verification_rounded),
+                          title: drawerTitle("Completed"),
+                          onTap: () {
+                            Navigator.pop(context);
+                          }),
+                      const SizedBox(height: 8.0),
+                      ListTile(
+                          leading: drawerIcon(Icons.delete_rounded),
+                          title: drawerTitle("Trash"),
+                          onTap: () {
+                            Navigator.pop(context);
+                          }),
+                      const SizedBox(height: 8.0),
+                      ListTile(
+                          leading: drawerIcon(Icons.settings),
+                          title: drawerTitle("Setting"),
+                          onTap: () {
+                            Navigator.pop(context);
+                          })
+                    ])))));
   }
 }
---
+++ lib/style/app.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+Color lightColor = const Color(0xFFEDF4F6);
+Color darkColor = const Color(0xFF1C1F20);
+Color lessDarkColor = const Color(0xFFD9D9D9);
+Color btnColor = const Color(0xFF0798C9);
+Color mainColor = const Color(0xFF0798C9);
+Color lighterMainColor = const Color(0xBF0798C9);
+Color lightestMainColor = const Color(0x4D0798C9);
+Color lightTextColor = const Color(0xBF1C1F20);
+
+AppBar theAppBar(String title) {
+  return AppBar(
+      leading: Builder(
+          builder: (context) => IconButton(
+              icon:
+                  const Icon(Icons.menu_rounded, color: Colors.black, size: 32),
+              onPressed: () => Scaffold.of(context).openDrawer())),
+      title: Text(title,
+          style: const TextStyle(
+              fontFamily: "Montserrat", fontSize: 28, color: Colors.black)),
+      elevation: 0.0,
+      backgroundColor: lightColor);
+}
+
+Icon drawerIcon(IconData icon) {
+  return Icon(icon, size: 40, color: darkColor);
+}
+
+Text drawerTitle(String title) {
+  return Text(title,
+      style:
+          TextStyle(fontFamily: "Montserrat", fontSize: 20, color: darkColor));
+}
