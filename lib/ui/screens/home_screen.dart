ea9053e813971b0b9befec92718745c18c7c880e
VieTie0910 <109495007+VieTie0910@users.noreply.github.com>
2022-10-24 10:14:46 +0700

    database_tabview

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import '../custom_tab_view.dart';
 import '../style/app.dart';
-import '../style/home_style.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,147 +13,143 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int initPosition = 0;
+
   @override
   Widget build(BuildContext context) {
     ///TabController
-    return DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          backgroundColor: lightColor,
-          appBar: theAppBar("Reminder"),
-          body: Container(
-              margin: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
-              child: Column(children: [
-                Row(children: [
+    return Scaffold(
+      backgroundColor: lightColor,
+      appBar: theAppBar("Reminder"),
+      body: Container(
+        margin: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+        child: SafeArea(
+          child: StreamBuilder<QuerySnapshot>(
+            stream: types.orderBy('created_time', descending: false).snapshots(),
+            builder: (context, snapshot) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
                   SizedBox(
-                      width: 292,
-                      child: TabBar(
-                          isScrollable: true,
-                          labelPadding:
-                              const EdgeInsets.only(left: 20.0, right: 20.0),
-                          indicatorSize: TabBarIndicatorSize.label,
-                          //indicatorPadding: EdgeInsets.all(16.0),
-                          indicator: BoxDecoration(
-                              color: lighterMainColor,
-                              borderRadius: const BorderRadius.all(
-                                  Radius.circular(16.0))),
-                          tabs: [
-                            Tab(child: label("All")),
-                            Tab(child: label("Home")),
-                            Tab(child: label("Task")),
-                            Tab(child: label("Shopping"))
-                          ])),
-                  // Button to add new type of note
-                  SizedBox(
-                    width: 68.0,
-                    child: FlatButton(
+                    width: 331.428,
+                    child: CustomTabView(
+                      initPosition: initPosition,
+                      itemCount: snapshot.data!.docs.length,
+                      tabBuilder: (context, index) => Tab(text: snapshot.data!.docs[index]['type_name']),
+                      pageBuilder: (context, index) => Center(child: Text(snapshot.data!.docs[index]['type_color'].toString())),
+                      onPositionChange: (index) {
+                        if (kDebugMode) {
+                          print('current position: $index');
+                        }
+                        initPosition = index;
+                      },
+                      onScroll: (position) => print('$position'),
+                    ),
+                  ),
+                    IconButton(
+                      padding: const EdgeInsets.all(0.0),
                         onPressed: () {},
-                        child: Icon(Icons.add_circle,
+                        icon: Icon(Icons.add_circle,
                             color: lighterMainColor, size: 38)),
-                  )
-                ]),
 
-                ///TabbarView
-                const SizedBox(
-                    height: 598.9,
-                    child: TabBarView(children: [
-                      Center(child: Text("All")),
-                      Center(child: Text("Home")),
-                      Center(child: Text("Task")),
-                      Center(child: Text("Shopping")),
-                    ]))
-              ])),
+                ],
+              );
+            }
+          ),
+        ),
+      ),
 
-          ///Drawer
-          drawer: Drawer(
-              child: Container(
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          colors: <Color>[
-                            lightestMainColor,
-                            lighterMainColor,
-                            mainColor
-                          ],
-                          tileMode: TileMode.mirror)),
-                  child: Padding(
-                      padding: EdgeInsets.zero,
-                      child: ListView(children: [
-                        SizedBox(
-                            height: 80.0,
-                            child: DrawerHeader(
-                                child: Center(
-                                    child: Text("T-Note",
-                                        style: TextStyle(
-                                            fontFamily: "Montserrat",
-                                            fontSize: 32,
-                                            color: darkColor))))),
+      ///Drawer
+      drawer: Drawer(
+          child: Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: <Color>[
+                        lightestMainColor,
+                        lighterMainColor,
+                        mainColor
+                      ],
+                      tileMode: TileMode.mirror)),
+              child: Padding(
+                  padding: EdgeInsets.zero,
+                  child: ListView(children: [
+                    SizedBox(
+                        height: 80.0,
+                        child: DrawerHeader(
+                            child: Center(
+                                child: Text("T-Note",
+                                    style: TextStyle(
+                                        fontFamily: "Montserrat",
+                                        fontSize: 32,
+                                        color: darkColor))))),
 
-                        /// All notes
-                        ListTile(
-                            leading: drawerIcon(Icons.event_note),
-                            title: drawerTitle("Notes"),
-                            onTap: () {
-                              Navigator.pop(context);
-                            }),
-                        const SizedBox(height: 8.0),
+                    /// All notes
+                    ListTile(
+                        leading: drawerIcon(Icons.event_note),
+                        title: drawerTitle("Notes"),
+                        onTap: () {
+                          Navigator.pop(context);
+                        }),
+                    const SizedBox(height: 8.0),
 
-                        /// Calendar
-                        ListTile(
-                            leading: drawerIcon(Icons.calendar_month),
-                            title: drawerTitle("Calendar"),
-                            onTap: () {
-                              Navigator.pop(context);
-                            }),
-                        const SizedBox(height: 8.0),
+                    /// Calendar
+                    ListTile(
+                        leading: drawerIcon(Icons.calendar_month),
+                        title: drawerTitle("Calendar"),
+                        onTap: () {
+                          Navigator.pop(context);
+                        }),
+                    const SizedBox(height: 8.0),
 
-                        /// Timeline
-                        ListTile(
-                            leading: drawerIcon(Icons.view_timeline),
-                            title: drawerTitle("Timeline"),
-                            onTap: () {
-                              Navigator.pop(context);
-                            }),
-                        const SizedBox(height: 8.0),
+                    /// Timeline
+                    ListTile(
+                        leading: drawerIcon(Icons.view_timeline),
+                        title: drawerTitle("Timeline"),
+                        onTap: () {
+                          Navigator.pop(context);
+                        }),
+                    const SizedBox(height: 8.0),
 
-                        /// All types of note
-                        ListTile(
-                            leading: drawerIcon(Icons.class_rounded),
-                            title: drawerTitle("Types"),
-                            onTap: () {
-                              Navigator.pop(context);
-                            }),
-                        const SizedBox(height: 8.0),
+                    /// All types of note
+                    ListTile(
+                        leading: drawerIcon(Icons.class_rounded),
+                        title: drawerTitle("Types"),
+                        onTap: () {
+                          Navigator.pop(context);
+                        }),
+                    const SizedBox(height: 8.0),
 
-                        /// All notes completed
-                        ListTile(
-                            leading:
-                                drawerIcon(Icons.domain_verification_rounded),
-                            title: drawerTitle("Completed"),
-                            onTap: () {
-                              Navigator.pop(context);
-                            }),
-                        const SizedBox(height: 8.0),
+                    /// All notes completed
+                    ListTile(
+                        leading:
+                        drawerIcon(Icons.domain_verification_rounded),
+                        title: drawerTitle("Completed"),
+                        onTap: () {
+                          Navigator.pop(context);
+                        }),
+                    const SizedBox(height: 8.0),
 
-                        /// All notes deleted
-                        ListTile(
-                            leading: drawerIcon(Icons.delete_rounded),
-                            title: drawerTitle("Trash"),
-                            onTap: () {
-                              Navigator.pop(context);
-                            }),
-                        const SizedBox(height: 8.0),
+                    /// All notes deleted
+                    ListTile(
+                        leading: drawerIcon(Icons.delete_rounded),
+                        title: drawerTitle("Trash"),
+                        onTap: () {
+                          Navigator.pop(context);
+                        }),
+                    const SizedBox(height: 8.0),
 
-                        /// Setting
-                        ListTile(
-                            leading: drawerIcon(Icons.settings),
-                            title: drawerTitle("Setting"),
-                            onTap: () {
-                              Navigator.pop(context);
-                            })
-                      ])))),
-          floatingActionButton: theFAB(),
-        ));
+                    /// Setting
+                    ListTile(
+                        leading: drawerIcon(Icons.settings),
+                        title: drawerTitle("Setting"),
+                        onTap: () {
+                          Navigator.pop(context);
+                        })
+                  ])))),
+      floatingActionButton: theFAB(),
+    );
   }
 }
